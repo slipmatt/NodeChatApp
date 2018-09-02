a68184090e27b846a3f71d971e19de0d633731b3
Imtiaz Buckas <imtiaz.pocketpc@gmail.com>
2018-09-02 14:30:41 +0200

    Post Message

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 // Routes
 app.use(express.static(__dirname))
-
+app.use(bodyParser.json())
 var messages=[
    {name: "Imtiaz",message: "Hello"},
    {name: "AI Machine",message: "Hi"},
 ]
+
 app.get('/messages', (req,res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 //listener
